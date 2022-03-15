c8b88e92b243bc28cf48f04a105642d73eb46d13
Tufail Ahmed <tufailahmedkhan@gmail.com>
2022-03-15 20:22:08 +0530

    second day commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector : 'our-first-app',
-    template : '<h1>Mohammad Tufail Ahmed - Trainer - JWA Batch</h1><emp-app></emp-app><cust-app></cust-app>'
+    template : '<h1>Mohammad Tufail Ahmed - Trainer - JWA Batch</h1><emp-app></emp-app>'
 })
 export class AppComponent
 {
--- src/app/customer.component.ts
+++ src/app/customer.component.ts
@@ -2,9 +2,22 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'cust-app',
-    template : '<h1>Customer App</h1>'
+    template : `<hr/><h1>Customer App</h1>
+                <h1>My second day learnings
+                    <br/>
+                {{ customerName }}
+                {{ 2+2 }}
+                {{ "Welcome "+ customerName + " and your bill amount is "+ billAmount}}
+            </h1>
+                `
 })
 export class CustomerComponent
 {
+    customerName:string = "Richard"
+    billAmount : number = 0;
 
+    constructor() {
+      //  this.customerName = "Daniel"
+        this.billAmount = 96000
+    }
 }
